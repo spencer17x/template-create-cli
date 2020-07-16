0aadd4ced0600236f8c732045ac74bf0c69e9175
Spencer17x <1253478653@qq.com>
2020-07-16 17:42:48 +0800

    fix: 🐛 删除eslint

--- commands/rui-temp.js
+++ commands/rui-temp.js
@@ -1,5 +1,15 @@
+const inquirer = require('inquirer');
+
 const questions = [
-	{}
+	{ name: 'projectName', message: '请输入项目名称', default: 'my-app' },
+	{ name: 'template', message: '请选择模板', type: 'list', choices: ['react-ui-template'] }
 ];
 
-console.log('welcome to init your template~~~');
+inquirer
+	.prompt(questions)
+	.then(answers => {
+		console.log('answers', answers);
+	})
+	.catch(error => {
+		console.log('error', error);
+	});
