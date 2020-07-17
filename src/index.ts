4fa0d648bbf7dc1da765ec75011951368c105390
Spencer17x <1253478653@qq.com>
2020-07-17 18:30:24 +0800

    fix: 🐛 阻止创建同名

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import * as inquirer from 'inquirer';
 import * as downloadGitRepo from 'download-git-repo';
 import * as ora from 'ora';
+import * as fs from 'fs';
 
 const questions = [
   { name: 'projectName', message: '请输入项目名称', default: 'my-app' },
@@ -32,27 +33,51 @@ class TemplateInit {
   /**
    * 初始化入口
    */
-  init() {
-    this.downloadRepo();
+  async init() {
+    const { answers: { template, projectName }, owner, branch } = this.options;
+    if (this.isDirExist(projectName)) {
+      console.log('当前路径下存在同名的目录，请重新创建项目');
+      return;
+    }
+    const spinner = ora('fetch template....').start();
+    const res = await this.downloadRepo(
+      template, projectName, owner, branch
+    );
+    spinner.stop();
+    console.log(res);
+  }
+
+  /**
+   * 检查目录是否已存在
+   * @param dir
+   */
+  isDirExist(dir: string) {
+    try {
+      fs.statSync(process.cwd() + '/' + dir);
+      return true;
+    } catch (e) {
+      return false;
+    }
   }
 
   /**
    * 拉取模板
    */
-  downloadRepo() {
-    const spinner = ora('fetch template....').start();
-    const { answers: { template, projectName }, owner, branch } = this.options;
-    const tempUrl = `https://github.com:${owner}/${template}#${branch}`;
-    console.log('\ntempUrl', tempUrl);
-    downloadGitRepo(
-      tempUrl,
-      projectName,
-      { clone: true },
-      (err: string) => {
-        console.log(err || 'Success');
-        spinner.stop();
-      }
-    );
+  downloadRepo(
+    template: string, projectName: string, owner: string, branch: string
+  ): Promise<string> {
+    return new Promise(resolve => {
+      const tempUrl = `https://github.com:${owner}/${template}#${branch}`;
+      console.log('\ntempUrl', tempUrl);
+      downloadGitRepo(
+        tempUrl,
+        projectName,
+        { clone: true },
+        (err: string) => {
+          resolve(err || 'Success');
+        }
+      );
+    });
   }
 }
 
